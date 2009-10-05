36c277f619476d85125f3592258d67fc78415ce5
kbr@chromium.org <kbr@chromium.org@642247e6-a953-0410-9eba-834bae0c6d03>
2009-10-05 19:12:14 +0000

    Copyright notices; whitespace fixes

--- samples/webgl/image-texture-test/demo.js
+++ samples/webgl/image-texture-test/demo.js
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2009 The Chromium Authors. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *    * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 var gl = null;
 var g_width = 0;
@@ -80,8 +109,8 @@ function initShaders() {
   }
   gl.attachShader(programObject, vertexShader);
   gl.attachShader(programObject, fragmentShader);
-  // Bind g_Position to attribute 0   
-  // Bind g_TexCoord0 to attribute 1   
+  // Bind g_Position to attribute 0
+  // Bind g_TexCoord0 to attribute 1
   gl.bindAttribLocation(programObject, 0, "g_Position");
   gl.bindAttribLocation(programObject, 1, "g_TexCoord0");
   // Link the program
