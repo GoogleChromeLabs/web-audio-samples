0ddb9c512c02aac43aef0eded27711765ea1cd97
crogers@google.com <crogers@google.com@642247e6-a953-0410-9eba-834bae0c6d03>
2010-02-26 22:33:27 +0000

    stop using jQuery... instead use <input type=range>

--- samples/audio/lib/events.js
+++ samples/audio/lib/events.js
@@ -47,7 +47,60 @@ function getRelativeCoordinates(eventInfo, opt_reference) {
 
 
 
+
   function addSlider(name) {
+    var controls = document.getElementById("controls");
+
+    var divName = name + "Slider";
+
+
+    var sliderText = '<div style="width:500px; height:20px;"> <input id="' + divName + '" '
+     + 'type="range" min="0" max="1" step="0.01" value="0" style="height: 20px; width: 450px;"> <div id="'
+     + name
+     + '-value" style="position:relative; left:30em; top:-18px;">'
+     + name
+     + '</div> </div> <br>  ';
+
+     // alert(sliderText);
+    
+    controls.innerHTML = controls.innerHTML + sliderText;
+  }
+
+
+  // <div style="width:500px;">
+  //     <input id="newSlider"
+  //            type="range" min="0" max="1" step="0.01" value="0" style="height: 20px; width: 450px;"
+  //            
+  //            onchange="newSliderChange(this.value);">
+  // 
+  //     <div id="newSlider-value"
+  //      style="position:relative; left:30em; top:-22px;"> 
+  // 
+  //      newSlider
+  //      </div>
+  // </div>
+  // <br>
+
+
+  function configureSlider(name, value, min, max, handler) {
+      // var controls = document.getElementById("controls");
+      // 
+
+      var divName = name + "Slider";
+
+      var slider = document.getElementById(divName);
+
+      // alert("slider = " + slider );
+      
+      slider.value = value;
+      slider.min = min;
+      slider.max = max;
+      slider.onchange = function() { handler(0, this); };
+      // slider.onchange = function() { alert("1"); }
+  }
+
+
+  function addSliderOld(name) {
     var controls = document.getElementById("controls");
 
     var divName = name + "Slider";
@@ -63,7 +116,7 @@ function getRelativeCoordinates(eventInfo, opt_reference) {
     controls.innerHTML = controls.innerHTML + sliderText;
   }
 
-  function configureSlider(name, value, min, max, handler) {
+  function configureSliderOld(name, value, min, max, handler) {
    var controls = document.getElementById("controls");
 
    var divName = name + "Slider";
