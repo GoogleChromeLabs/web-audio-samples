b06210e3c2bb78cbfab72750d7840c48fb7fe199
crogers@google.com <crogers@google.com@642247e6-a953-0410-9eba-834bae0c6d03>
2011-07-06 20:20:20 +0000

    add getAbsolutePosition()

--- samples/audio/lib/events.js
+++ samples/audio/lib/events.js
@@ -1,3 +1,14 @@
+function getAbsolutePosition(element) {
+  var r = { x: element.offsetLeft, y: element.offsetTop };
+  if (element.offsetParent) {
+    var tmp = getAbsolutePosition(element.offsetParent);
+    r.x += tmp.x;
+    r.y += tmp.y;
+  }
+  return r;
+};
+
+
 function getRelativeCoordinates(eventInfo, opt_reference) {
     var x, y;
     var event = eventInfo.event;
