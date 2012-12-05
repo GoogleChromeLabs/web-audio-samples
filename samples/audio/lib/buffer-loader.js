d9d222d35d879ca277f07161a7003c369ee59dcd
crogers@google.com <crogers@google.com@642247e6-a953-0410-9eba-834bae0c6d03>
2012-12-05 01:12:17 +0000

    update BufferLoader

--- samples/audio/lib/buffer-loader.js
+++ samples/audio/lib/buffer-loader.js
@@ -1,44 +1,42 @@
 function BufferLoader(context, urlList, callback) {
-    this.context = context;
-    this.urlList = urlList;
-    this.onload = callback;
-    this.bufferList = new Array();
-    this.loadCount = 0;
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
 }
 
 BufferLoader.prototype.loadBuffer = function(url, index) {
-    // Load buffer asynchronously
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
 
-    var loader = this;
+  var loader = this;
 
-    request.onload = function() {
-        var buffer;
-        try {
-            buffer = loader.context.createBuffer(request.response, false);
-        } catch(e) {
-            alert('error decoding file data: ' + url);
-        }
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+    request.response, function(buffer) {
+      if (!buffer) {
+        alert('error decoding file data: ' + url);
+        return;
+      }
+      loader.bufferList[index] = buffer;
+      if (++loader.loadCount == loader.urlList.length) loader.onload(loader.bufferList);
+    }, function(error) {
+      console.error('decodeAudioData error', error);
+    });
+  }
 
-        try {
-            loader.bufferList[index] = buffer;
-            if (++loader.loadCount == loader.urlList.length)
-                loader.onload(loader.bufferList);
-        } catch(e) {
-            alert('BufferLoader: callback problem');
-        }
-    }
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
 
-    request.onerror = function() {
-        alert('BufferLoader: XHR error');        
-    }
-
-    request.send();
+  request.send();
 }
 
 BufferLoader.prototype.load = function() {
-    for (var i = 0; i < this.urlList.length; ++i)
-        this.loadBuffer(this.urlList[i], i);
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
 }
