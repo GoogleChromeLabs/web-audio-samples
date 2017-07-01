f608a9c3c6ea2237068557f14ab63aaaae537814
samhincks <hincks@google.com>
2017-06-30 18:28:57 -0700

    Bitcrusher audio worklet currently non-functional. Need to change audio params

---
+++ audio-worklet/src/bitcrusher-audio-worklet.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @class Bitcrusher
+ * @extends AudioWorkletProcessor
+ * The bitcrusher reduces the sample rate and bit depth of an audiobuffer.
+ */
+registerProcessor('bitcrusher-audio-worklet', class BitcrusherAudioWorklet extends AudioWorkletProcessor {
+
+  static get parameterDescriptors () {
+    return [{
+      name: 'bitDepth',
+      defaultValue: 16,
+      minValue: 1,
+      maxValue: 24
+    }, {
+      name: 'reduction',
+      defaultValue: 1,
+      minValue: 1,
+      maxValue: 10
+    }];
+  }
+
+  constructor(options) {
+    super(options);
+
+    // Index and previousSample defined as globals to handle block transitions.
+    this.index_ = 0;
+    this.previousSample_ = 0;
+  }
+
+  
+  /**
+   * Bit crush upon receiving input audio signal, applying signal distortion
+   * effects to the output buffer.
+   * @param  {AudioProcessingEvent} event holds input and output buffers
+   */
+  process(input, output, parameters) {
+    let inputChannelData = input.getChannelData(0);
+    let outputChannelData = output.getChannelData(0);
+    const scale = Math.pow(2, parameters.bitDepth);
+
+    // Add new bit crushed sample to outputBuffer at specified interval.
+    for (let j = 0; j < inputChannelData.length; j++) {
+      if (this.index_ % parameters.reduction === 0) {
+        // Scale up and round off low order bits.
+        const rounded = Math.round(inputChannelData[j] * scale);
+        this.previousSample_ = rounded / scale;
+      }
+      outputChannelData[j] = this.previousSample_;
+      this.index_++;
+    }
+    //console.log(outputChannelData);
+    //console.log(inputChannelData, outputChannelData, parameters);
+  }
+});
