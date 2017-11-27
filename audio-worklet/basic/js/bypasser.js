8929141809960d0fd1fad90a5a8882665ba14736
Hongchan Choi <hongchan.choi@gmail.com>
2017-11-27 11:26:36 -0800

    Revert changes in bypasser.js

--- audio-worklet/basic/js/bypasser.js
+++ audio-worklet/basic/js/bypasser.js
@@ -5,14 +5,20 @@
  * This processor class is for the life cycle and the processor state event.
  * It only lives for 1 render quantum.
  */
-class TimedProcessor extends AudioWorkletProcessor {
+class Bypasser extends AudioWorkletProcessor {
   constructor() {
     super();
-    this.createdAt_ = currentTime;
-    this.lifetime_ = 128 / sampleRate;
   }
 
-  process() {
-    return currentTime - this.createdAt_ > this.lifetime_ ? false : true;
+  process(inputs, outputs) {
+    let input = inputs[0];
+    let output = outputs[0];
+    for (let channel = 0; channel < output.length; ++channel) {
+      output[channel].set(input[channel]);
+    }
+
+    return true;
   }
 }
+
+registerProcessor('bypasser', Bypasser);
