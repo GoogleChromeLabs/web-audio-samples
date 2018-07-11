37a050f87dd76ec44b04f1f4db5e6370935087a3
Hongchan Choi <hongchan.choi@gmail.com>
2018-07-11 10:18:22 -0700

    changing license to Apache 2 and address comments from rtoy@

--- audio-worklet/design-pattern/shared-buffer/shared-buffer-worker.js
+++ audio-worklet/design-pattern/shared-buffer/shared-buffer-worker.js
@@ -1,9 +1,18 @@
 /**
- * Copyright (c) 2018 The Chromium Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 
 // This Worker is the actual backend of AudioWorkletProcessor (AWP). After
 // instantiated/initialized by AudioWorkletNode (AWN), it communicates with the
@@ -129,8 +138,9 @@ function initialize(options) {
   if (!self.SharedArrayBuffer) {
     postMessage({
       message: 'WORKER_ERROR',
-      detail: `SharedArrayBuffer is not supported. See the article above to
-          enable the feature.`,
+      detail: `SharedArrayBuffer is not supported in your browser. See
+          https://developers.google.com/web/updates/2018/06/audio-worklet-design-pattern
+          for more info.`,
     });
     return;
   }
