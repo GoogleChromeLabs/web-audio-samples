28774c4c00e235b0f9481251555b3f18e37bde85
Gaoping Huang <gaopinghuang@gmail.com>
2019-07-09 13:45:13 -0700

    Add all source code of Vocoder

---
+++ geiger/Vocoder/js/waveshaper.js
@@ -0,0 +1,107 @@
+// Copyright 2011, Google Inc.
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+var threshold = -27; // dB
+var headroom = 21; // dB
+
+function e4(x, k)
+{
+    return 1.0 - Math.exp(-k * x);
+}
+
+function dBToLinear(db) {
+    return Math.pow(10.0, 0.05 * db);
+}
+
+function shape(x) {
+    var linearThreshold = dBToLinear(threshold);
+    var linearHeadroom = dBToLinear(headroom);
+    
+    var maximum = 1.05 * linearHeadroom * linearThreshold;
+    var kk = (maximum - linearThreshold);
+    
+    var sign = x < 0 ? -1 : +1;
+    var absx = Math.abs(x);
+    
+    var shapedInput = absx < linearThreshold ? absx : linearThreshold + kk * e4(absx - linearThreshold, 1.0 / kk);
+    shapedInput *= sign;
+    
+    return shapedInput;
+}
+
+function generateColortouchCurve(curve) {
+    var n = 65536;
+    var n2 = n / 2;
+    
+    for (var i = 0; i < n2; ++i) {
+        x = i / n2;
+        x = shape(x);
+        
+        curve[n2 + i] = x;
+        curve[n2 - i - 1] = -x;
+    }
+    
+    return curve;
+}
+
+function generateMirrorCurve(curve) {
+    var n = 65536;
+    var n2 = n / 2;
+    
+    for (var i = 0; i < n2; ++i) {
+        x = i / n2;
+        x = shape(x);
+        
+        curve[n2 + i] = x;
+        curve[n2 - i - 1] = x;
+    }
+    
+    return curve;
+}
+
+function WaveShaper(context) {
+    this.context = context;
+    var waveshaper = context.createWaveShaper();
+    var preGain = context.createGain();
+    var postGain = context.createGain();
+    preGain.connect(waveshaper);
+    waveshaper.connect(postGain);
+    this.input = preGain;
+    this.output = postGain;
+    
+    var curve = new Float32Array(65536); // FIXME: share across instances
+    generateColortouchCurve(curve);
+    waveshaper.curve = curve;
+}
+
+WaveShaper.prototype.setDrive = function(drive) {
+    this.input.gain.value = drive;
+    var postDrive = Math.pow(1 / drive, 0.6);
+    this.output.gain.value = postDrive;
+}
---
+++ geiger/Vocoder/shaders/sonogram-vertex.shader
@@ -0,0 +1,59 @@
+// The vertex shader used for the 3D sonogram visualization
+
+attribute vec3 gPosition;
+attribute vec2 gTexCoord0;
+uniform sampler2D vertexFrequencyData;
+uniform float vertexYOffset;
+uniform mat4 worldViewProjection;
+uniform float verticalScale;
+
+varying vec2 texCoord;
+varying vec3 color;
+
+
+
+/**
+ * Conversion based on Wikipedia article
+ * @see http://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
+ */
+vec3 convertHSVToRGB(in float hue, in float saturation, in float lightness) {
+  float chroma = lightness * saturation;
+  float hueDash = hue / 60.0;
+  float x = chroma * (1.0 - abs(mod(hueDash, 2.0) - 1.0));
+  vec3 hsv = vec3(0.0);
+
+  if(hueDash < 1.0) {
+    hsv.r = chroma;
+    hsv.g = x;
+  } else if (hueDash < 2.0) {
+    hsv.r = x;
+    hsv.g = chroma;
+  } else if (hueDash < 3.0) {
+    hsv.g = chroma;
+    hsv.b = x;
+  } else if (hueDash < 4.0) {
+    hsv.g = x;
+    hsv.b = chroma;
+  } else if (hueDash < 5.0) {
+    hsv.r = x;
+    hsv.b = chroma;
+  } else if (hueDash < 6.0) {
+    hsv.r = chroma;
+    hsv.b = x;
+  }
+
+  return hsv;
+}
+
+
+void main()
+{
+    float x = pow(256.0, gTexCoord0.x - 1.0);
+    vec4 sample = texture2D(vertexFrequencyData, vec2(x, gTexCoord0.y + vertexYOffset));
+    vec4 newPosition = vec4(gPosition.x, gPosition.y + verticalScale * sample.a, gPosition.z, 1.0);
+    gl_Position = worldViewProjection * newPosition;
+    texCoord = gTexCoord0;
+
+    float hue = 360.0 - ((newPosition.y / verticalScale) * 360.0);
+    color = convertHSVToRGB(hue, 1.0, 1.0);
+}
