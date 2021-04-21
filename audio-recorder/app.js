addeb3b2d5b6f6da717aba080b1b039840d8ac02
Max Vogler <maxvo@google.com>
2021-04-21 12:58:36 +0200

    Refactor code and add comments.

--- audio-recorder/app.js
+++ audio-recorder/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const DELETE_BUTTON_SELECTOR = ".delete-button";
+
 const recordButton = document.querySelector("#record");
 const recordOutlineEl = document.querySelector("#record-outline");
 const soundClips = document.querySelector(".sound-clips");
@@ -8,6 +10,7 @@ const clipTemplate = document.querySelector("#clip-template");
 new mdc.iconButton.MDCIconButtonToggle(recordButton);
 recordButton.onclick = startRecording;
 
+/** Accesses the device's microphone and returns an audio stream (or null on error). */
 async function getAudioStream() {
   try {
     return await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -17,33 +20,38 @@ async function getAudioStream() {
   }
 }
 
+/** Starts recording an audio snippet in-memory and visualizes the recording waveform.  */
 async function startRecording() {
   const chunks = [];
   const stream = await getAudioStream();
   if (!stream) {
-    return;
+    return; // Permissions have not been granted or an error occurred.
   }
 
-  const mediaRecorder = new MediaRecorder(stream);
-
+  // Insert a new audio clip at the top of the list.
   const clipContainer = clipTemplate.content.firstElementChild.cloneNode(true);
-  clipContainer.querySelector(".delete-button").onclick = () => {
+  clipContainer.querySelector(DELETE_BUTTON_SELECTOR).onclick = () => {
     clipContainer.parentNode.removeChild(clipContainer);
   };
   soundClips.prepend(clipContainer);
 
+  // Start recording the microphone's audio stream in-memory.
+  const mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.ondataavailable = ({ data }) => {
+    chunks.push(data);
+  };
   mediaRecorder.onstop = () => {
     onRecordingStopped({ clipContainer, chunks });
   };
-  mediaRecorder.ondataavailable = (e) => {
-    chunks.push(e.data);
-  };
-
-  recordButton.onclick = () => mediaRecorder.stop();
   mediaRecorder.start();
+
+  recordButton.onclick = () => {
+    mediaRecorder.stop();
+  };
   visualizeRecording({ stream, clipContainer, mediaRecorder });
 }
 
+/** Finalizes the audio recording.. */
 function onRecordingStopped({ clipContainer, chunks }) {
   const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
   chunks = [];
@@ -53,6 +61,7 @@ function onRecordingStopped({ clipContainer, chunks }) {
   recordButton.onclick = startRecording;
 }
 
+/** Visualizes the audio with a waveform and a loudness indicator. */
 function visualizeRecording({ stream, clipContainer, mediaRecorder }) {
   const canvas = clipContainer.querySelector("canvas");
   canvas.width = clipContainer.offsetWidth;
@@ -61,49 +70,56 @@ function visualizeRecording({ stream, clipContainer, mediaRecorder }) {
   canvasCtx.fillStyle = "#263238";
   canvasCtx.setLineDash([2, 5]);
 
+  // Use AnalyserNode to compute the recorded audio's power to visualize loudness.
   const audioCtx = new AudioContext();
   const source = audioCtx.createMediaStreamSource(stream);
   const analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 32;
+  analyser.fftSize = 32; // Smallest possible FFT size for cheaper computation.
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
 
   source.connect(analyser);
 
-  let x = -1;
+  // Horizontal position in the canvas for drawing the waveform.
+  let x = 0;
 
+  /** Repeatedly draws the waveform and loudness indicator. */
   function draw() {
     const width = canvas.width;
     const height = canvas.height;
 
+    // Read the maximum power from the FFT ana
     analyser.getByteFrequencyData(dataArray);
     const volume = Math.max(...dataArray) / 255;
 
     if (mediaRecorder.state !== "recording") {
       recordOutlineEl.style.boxShadow = "none";
-      return; // Stop drawing loop.
+      return; // Stop drawing loop once MediaRecorder stopped.
     }
 
-    recordOutlineEl.style.boxShadow = `0 0 0 ${
-      volume * 20
-    }px rgba(0, 0, 0, 0.2)`;
+    // Visualize current loudness through a ring around the record button.
+    const radius = volume * 20;
+    recordOutlineEl.style.boxShadow = `0 0 0 ${radius}px rgba(0, 0, 0, 0.2)`;
 
-    x = Math.min(x + 1, width - 1);
-
-    if (x <= 0) {
+    // Before drawing the first waveform, draw a horizontal, dashed line in the center of the canvas.
+    if (x == 0) {
       canvasCtx.beginPath();
       canvasCtx.moveTo(0, height / 2);
       canvasCtx.lineTo(width, height / 2);
       canvasCtx.stroke();
     }
 
-    if (x >= width - 1) {
+    // Append a vertical line on the right of the waveform, that indicates the loudness.
+    canvasCtx.fillRect(x, ((1 - volume) * height) / 2, 1, volume * height);
+
+    if (x < width - 1) {
+      x++;
+    } else {
+      // If the waveform fills the canvas, move it by one pixel to the left to make room.
       canvasCtx.globalCompositeOperation = "copy";
       canvasCtx.drawImage(canvas, -1, 0);
       canvasCtx.globalCompositeOperation = "source-over";
     }
-
-    canvasCtx.fillRect(x, ((1 - volume) * height) / 2, 1, volume * height);
 
     requestAnimationFrame(draw);
   }
