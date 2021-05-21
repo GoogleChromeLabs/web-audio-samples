b3a65048a51bb87db52eda5f35576cd9acd3d5ac
Hongchan Choi <hongchan.choi@gmail.com>
2021-05-21 09:23:18 -0700

    Reindent

--- home/PageData.js
+++ home/PageData.js
@@ -13,126 +13,126 @@
  * limitations under the License.
  */
 
- const GitHubRepo = '//googlechromelabs.github.io/web-audio-samples/';
+const GitHubRepo = '//googlechromelabs.github.io/web-audio-samples/';
 
- export default {
-   Resources: {
-     listTitle: 'Tools and Resources',
-     listData: [
-       {
-         title: 'Audio Worklet',
-         description: 'Audio Worklet examples and resources',
-         url: 'audio-worklet/',
-       },
-       {
-         title: 'Omnitone',
-         description: '360° audio renderer for WebXR applications',
-         url: '//googlechrome.github.io/omnitone/',
-       },
-       {
-         title: 'Web Audio Inspector DevTools Extension',
-         description: 'Chrome DevTools extension for Web Audio API',
-         url: '//github.com/google/audion/',
-       },
-       {
-         title: 'Canopy',
-         description: 'Web audio code editor for tinkering and debugging',
-         url: '//hoch.github.io/canopy/',
-       },
-       {
-         title: 'Web Audio API 드럼머신 만들기',
-         description: 'Mother Language Day Special: Drum Sampler Tutorial',
-         url: 'drum-machine/',
-       },
-       {
-         title: 'Audio Recorder',
-         description: 'A simple PWA that records audio clips',
-         url: 'audio-recorder/',
-       },
-       {
+export default {
+  Resources: {
+    listTitle: 'Tools and Resources',
+    listData: [
+      {
+        title: 'Audio Worklet',
+        description: 'Audio Worklet examples and resources',
+        url: 'audio-worklet/',
+      },
+      {
+        title: 'Omnitone',
+        description: '360° audio renderer for WebXR applications',
+        url: '//googlechrome.github.io/omnitone/',
+      },
+      {
+        title: 'Web Audio Inspector DevTools Extension',
+        description: 'Chrome DevTools extension for Web Audio API',
+        url: '//github.com/google/audion/',
+      },
+      {
+        title: 'Canopy',
+        description: 'Web audio code editor for tinkering and debugging',
+        url: '//hoch.github.io/canopy/',
+      },
+      {
+        title: 'Web Audio API 드럼머신 만들기',
+        description: 'Mother Language Day Special: Drum Sampler Tutorial',
+        url: 'drum-machine/',
+      },
+      {
+        title: 'Audio Recorder',
+        description: 'A simple PWA that records audio clips',
+        url: 'audio-recorder/',
+      },
+      {
         title: 'Web Audio API Specification',
         description: 'Latest version of API specification',
         url: '//webaudio.github.io/web-audio-api/',
       },
-     ],
-   },
-   Projects: {
-     listTitle: 'Selected Projects',
-     listData: [
-       {
-         title: 'Tone.js',
-         description: 'Framework for creating interactive music in browser',
-         url: '//tonejs.github.io/',
-       },
-       {
-         title: 'Resonance Audio for Web',
-         description: 'SDK for interactive ambisonic soundfield',
-         url: '//developers.google.com/resonance-audio/develop/web/getting-started',
-       },
-       {
-         title: 'Chrome Music Lab',
-         description: 'Learning music through web technology',
-         url: '//musiclab.chromeexperiments.com/',
-       },
-       {
-         title: 'Google Doodle: Hiphop',
-         description: '44th Anniversary of the Birth of Hip Hop',
-         url: '//www.google.com/doodles/44th-anniversary-of-the-birth-of-hip-hop',
-       },
-       {
-         title: 'Google Doodle: Clara Rockmore',
-         description: 'Clara Rockmore’s 105th Birthday',
-         url: '//www.google.com/doodles/clara-rockmores-105th-birthday',
-       },
-       {
-         title: 'Google Doodle: Robert Moog’s 78th Birthday',
-         description: 'Robert Moog’s 78th Birthday',
-         url: '//www.google.com/doodles/robert-moogs-78th-birthday',
-       },
-       {
-         title: 'Web Audio Demos',
-         description: 'A demo collection by Chris Wilson',
-         url: '//webaudiodemos.appspot.com/',
-       },
-       {
-         title: 'Filter Playground',
-         description: 'Interactive filter design tool by Boris Smus',
-         url: '//borismus.github.io/filter-playground/',
-       },
-       {
-         title: 'Beautiful Audio Editor',
-         description: 'Multitrack recorder/editor by Chi Zeng',
-         url: '//beautifulaudioeditor.appspot.com/',
-       },
-     ]
-   },
-   Demos: {
-     listTitle: 'Demos',
-     listData: [
-       {
-         title: 'Old demo collection',
-         url: 'archive/demos/',
-       },
-       {
-         title: 'Box2D Stress Test',
-         url: 'archive/stress-box/',
-       },
-       {
-         title: 'Wavetable Synthesizer',
-         url: 'archive/demos/wavetable-synth.html',
-       },
-       {
-         title: 'AnalyserNode with visualization ',
-         url: 'archive/demos/visualizer-gl.html',
-       },
-       {
-         title: 'Panning and Reverberation',
-         url: 'archive/demos/simple.html',
-       },
-       {
-         title: '8 Ball with 3D audio',
-         url: 'archive/demos/o3d-webgl-samples/pool.html',
-       }
-     ]
-   },
- };
+    ],
+  },
+  Projects: {
+    listTitle: 'Selected Projects',
+    listData: [
+      {
+        title: 'Tone.js',
+        description: 'Framework for creating interactive music in browser',
+        url: '//tonejs.github.io/',
+      },
+      {
+        title: 'Resonance Audio for Web',
+        description: 'SDK for interactive ambisonic soundfield',
+        url: '//developers.google.com/resonance-audio/develop/web/getting-started',
+      },
+      {
+        title: 'Chrome Music Lab',
+        description: 'Learning music through web technology',
+        url: '//musiclab.chromeexperiments.com/',
+      },
+      {
+        title: 'Google Doodle: Hiphop',
+        description: '44th Anniversary of the Birth of Hip Hop',
+        url: '//www.google.com/doodles/44th-anniversary-of-the-birth-of-hip-hop',
+      },
+      {
+        title: 'Google Doodle: Clara Rockmore',
+        description: 'Clara Rockmore’s 105th Birthday',
+        url: '//www.google.com/doodles/clara-rockmores-105th-birthday',
+      },
+      {
+        title: 'Google Doodle: Robert Moog’s 78th Birthday',
+        description: 'Robert Moog’s 78th Birthday',
+        url: '//www.google.com/doodles/robert-moogs-78th-birthday',
+      },
+      {
+        title: 'Web Audio Demos',
+        description: 'A demo collection by Chris Wilson',
+        url: '//webaudiodemos.appspot.com/',
+      },
+      {
+        title: 'Filter Playground',
+        description: 'Interactive filter design tool by Boris Smus',
+        url: '//borismus.github.io/filter-playground/',
+      },
+      {
+        title: 'Beautiful Audio Editor',
+        description: 'Multitrack recorder/editor by Chi Zeng',
+        url: '//beautifulaudioeditor.appspot.com/',
+      },
+    ]
+  },
+  Demos: {
+    listTitle: 'Demos',
+    listData: [
+      {
+        title: 'Old demo collection',
+        url: 'archive/demos/',
+      },
+      {
+        title: 'Box2D Stress Test',
+        url: 'archive/stress-box/',
+      },
+      {
+        title: 'Wavetable Synthesizer',
+        url: 'archive/demos/wavetable-synth.html',
+      },
+      {
+        title: 'AnalyserNode with visualization ',
+        url: 'archive/demos/visualizer-gl.html',
+      },
+      {
+        title: 'Panning and Reverberation',
+        url: 'archive/demos/simple.html',
+      },
+      {
+        title: '8 Ball with 3D audio',
+        url: 'archive/demos/o3d-webgl-samples/pool.html',
+      }
+    ]
+  },
+};
