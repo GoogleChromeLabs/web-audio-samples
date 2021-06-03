fe139332018b6d734573a0eb6f87c73f708b0c70
Max Vogler <maxvo@google.com>
2021-06-03 16:23:59 +0200

    Refactor and extract visualization to dedicated file.

--- audio-recorder/app.mjs
+++ audio-recorder/app.mjs
@@ -1,6 +1,7 @@
 'use strict';
 
 import {IndexedDBStorage} from './indexeddb-storage.mjs';
+import * as visualize from './visualize.mjs';
 
 const DELETE_BUTTON_SELECTOR = '.delete-button';
 
@@ -73,6 +74,12 @@ async function startRecording({storage}) {
   }
 
   const clipContainer = insertClip();
+  const canvas = clipContainer.querySelector('canvas');
+  canvas.width = clipContainer.offsetWidth;
+
+  const outlineIndicator = new visualize.OutlineLoudnessIndicator(
+      recordOutlineEl);
+  const waveformIndicator = new visualize.WaveformIndicator(canvas);
 
   // Start recording the microphone's audio stream in-memory.
   const mediaRecorder = new MediaRecorder(stream);
@@ -80,6 +87,7 @@ async function startRecording({storage}) {
     chunks.push(data);
   };
   mediaRecorder.onstop = async () => {
+    outlineIndicator.hide();
     recordButton.onclick = () => startRecording({storage});
     const blob = new Blob(chunks, {type: mediaRecorder.mimeType});
     const id = await storage.save(blob);
@@ -94,18 +102,12 @@ async function startRecording({storage}) {
       track.stop();
     });
   };
-  visualizeRecording({stream, clipContainer});
+
+  visualizeRecording({stream, outlineIndicator, waveformIndicator});
 }
 
 /** Visualizes the audio with a waveform and a loudness indicator. */
-function visualizeRecording({stream, clipContainer}) {
-  const canvas = clipContainer.querySelector('canvas');
-  canvas.width = clipContainer.offsetWidth;
-
-  const canvasCtx = canvas.getContext('2d');
-  canvasCtx.fillStyle = '#263238';
-  canvasCtx.setLineDash([2, 5]);
-
+function visualizeRecording({stream, outlineIndicator, waveformIndicator}) {
   // Use AnalyserNode to compute the recorded audio's power to visualize
   // loudness.
   const audioCtx = new AudioContext();
@@ -117,49 +119,18 @@ function visualizeRecording({stream, clipContainer}) {
 
   source.connect(analyser);
 
-  // Horizontal position in the canvas for drawing the waveform.
-  let x = 0;
+  waveformIndicator.drawCenterLine();
 
   /** Repeatedly draws the waveform and loudness indicator. */
   function draw() {
     if (!stream.active) {
-      recordOutlineEl.style.boxShadow = 'none';
       return; // Stop drawing loop once the recording stopped.
     }
 
-    const width = canvas.width;
-    const height = canvas.height;
-
-    // Read the maximum power from the FFT ana
     analyser.getByteFrequencyData(dataArray);
-    const volume = Math.max(...dataArray) / 255;
-
-    // Visualize current loudness through a ring around the record button.
-    const radius = volume * 20;
-    recordOutlineEl.style.boxShadow = `0 0 0 ${radius}px rgba(0, 0, 0, 0.2)`;
-
-    // Before drawing the first waveform, draw a horizontal, dashed line in the
-    // center of the canvas.
-    if (x == 0) {
-      canvasCtx.beginPath();
-      canvasCtx.moveTo(0, height / 2);
-      canvasCtx.lineTo(width, height / 2);
-      canvasCtx.stroke();
-    }
-
-    // Append a vertical line on the right of the waveform, that indicates the
-    // loudness.
-    canvasCtx.fillRect(x, ((1 - volume) * height) / 2, 1, volume * height);
-
-    if (x < width - 1) {
-      x++;
-    } else {
-      // If the waveform fills the canvas, move it by one pixel to the left to
-      // make room.
-      canvasCtx.globalCompositeOperation = 'copy';
-      canvasCtx.drawImage(canvas, -1, 0);
-      canvasCtx.globalCompositeOperation = 'source-over';
-    }
+    const loudness = visualize.calculateLoudness(dataArray);
+    outlineIndicator.show(loudness);
+    waveformIndicator.show(loudness);
 
     requestAnimationFrame(draw);
   }
