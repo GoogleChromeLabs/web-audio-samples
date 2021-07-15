b24688a3c8fdefc11bf46107a22af7b2213fa688
Max Vogler <maxvo@google.com>
2021-07-15 15:34:25 +0200

    Refactor to modern ES.

--- archive/demos/dj.js
+++ archive/demos/dj.js
@@ -1,69 +1,32 @@
 /* eslint require-jsdoc: "off" */
 /* global addSlider configureSlider */ // loaded from lib/events.js.
 
-// Temporary patch until all browsers support unprefixed context.
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
 // init() once the page has finished loading.
 window.onload = init;
 
 let context;
-let source1 = 0;
-let source2 = 0;
-let source1Gain = 0;
-let source2Gain = 0;
-let convolver;
-let compressor;
+let source1;
+let source2;
+let source1Gain;
+let source2Gain;
 let wetGainNode1;
 let wetGainNode2;
 let lowFilter1;
 let lowFilter2;
-let preCompressorGain;
-let postCompressorGain;
 
-let bufferLoader1 = 0;
-let bufferLoader2 = 0;
-let loadCount = 0;
 const tempo = 120.0; // hardcoded for now
 let anchorTime = 0;
 
-/**
- * Class BufferLoader
- */
 
-const BufferLoader = function(source, url) {
-  this.source_ = source;
-  this.url_ = url;
-  this.buffer_ = 0;
-  this.request_ = 0;
-};
+async function fetchAndDecodeAudio(url) {
+  const response = await fetch(url);
+  const responseBuffer = await response.arrayBuffer();
 
-BufferLoader.prototype.onFileLoaded = function(buffer) {
-  this.buffer = buffer;
-  this.source_.buffer = buffer;
-  loadCount++;
-  if (loadCount == 2) {
-    finishLoad();
-  }
-};
-
-BufferLoader.prototype.load = function() {
-  // Load asynchronously
-  const request = new XMLHttpRequest();
-  request.open('GET', this.url_, true);
-  request.responseType = 'arraybuffer';
-  this.request_ = request;
-
-  request.onload = function() {
-    context.decodeAudioData(request.response, this.onFileLoaded.bind(this),
-        function() {
-          console.log('error decoding file.');
-        });
-  }.bind(this);
-
-  request.send();
-};
-
+  // TODO: Migrate to Promise-syntax once Safari supports it.
+  return new Promise((resolve, reject) => {
+    context.decodeAudioData(responseBuffer, resolve, reject);
+  });
+}
 
 function crossfadeHandler(event, ui) {
   const x = ui.value;
@@ -123,116 +86,44 @@ function rightRhythmEffectHandler(event, ui) {
   wetGainNode2.gain.value = value;
 }
 
-function finishLoad() {
-  // first, get rid of loading animation
-  const loading = document.getElementById('loading');
-  loading.innerHTML = '';
+window.loadBufferForSource = async (sourceName, url) => {
+  const buffer = await fetchAndDecodeAudio(url);
 
-  addSlider('crossfade');
-  addSlider('leftFilter');
-  addSlider('rightFilter');
-  addSlider('leftRhythmEffect');
-  addSlider('rightRhythmEffect');
-  configureSlider('crossfade', 0.5, 0.0, 1.0, crossfadeHandler);
-  configureSlider('leftFilter', 0.99, 0.0, 1.0, leftFilterHandler);
-  configureSlider('rightFilter', 0.99, 0.0, 1.0, rightFilterHandler);
-  configureSlider('leftRhythmEffect', 0.0, 0.0, 1.0, leftRhythmEffectHandler);
-  configureSlider('rightRhythmEffect', 0.0, 0.0, 1.0, rightRhythmEffectHandler);
+  // Start playing the new buffer at exactly the next 4-beat boundary
+  let currentTime = context.currentTime;
 
-  const now = context.currentTime;
-  anchorTime = now + 0.040;
+  // Add 120ms slop since we don't want to schedule too soon into the
+  // future. This will potentially cause us to wait 4 more beats, if we're
+  // almost exactly at the next 4-beat transition.
+  currentTime += 0.120;
 
-  source1.start(anchorTime);
-  source2.start(anchorTime);
+  const delta = currentTime - anchorTime;
+  const deltaBeat = (tempo / 60.0) * delta;
+  const roundedUpDeltaBeat = 4.0 + 4.0 * Math.floor(deltaBeat / 4.0);
+  const roundedUpDeltaTime = (60.0 / tempo) * roundedUpDeltaBeat;
+  const time = anchorTime + roundedUpDeltaTime;
 
-  draw();
-}
+  const newSource = context.createBufferSource();
 
-function setReverbImpulseResponse(url) {
-  // Load impulse response asynchronously
+  if (sourceName === 'source1') {
+    // Stop the current loop (when it gets to the next 4-beat boundary).
+    source1.stop(time);
+    // This new source will replace the existing source.
+    newSource.connect(source1Gain);
+    source1 = newSource;
+  } else {
+    source2.stop(time);
+    newSource.connect(source2Gain);
+    source2 = newSource;
+  }
 
-  const request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
+  // Assign the buffer to the new source.
+  newSource.buffer = buffer;
 
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      convolver.buffer=buffer;
-    }, function() {
-      console.log('error decoding file.');
-    });
-  };
-
-  request.send();
-}
-
-window.loadBufferForSource = function(source, url) {
-  // Load asynchronously
-
-  const request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      // Start playing the new buffer at exactly the next 4-beat boundary
-      let currentTime = context.currentTime;
-
-      // Add 120ms slop since we don't want to schedule too soon into the
-      // future. This will potentially cause us to wait 4 more beats, if we're
-      // almost exactly at the next 4-beat transition.
-      currentTime += 0.120;
-
-      const delta = currentTime - anchorTime;
-      const deltaBeat = (tempo / 60.0) * delta;
-      const roundedUpDeltaBeat = 4.0 + 4.0 * Math.floor(deltaBeat / 4.0);
-      const roundedUpDeltaTime = (60.0 / tempo) * roundedUpDeltaBeat;
-      const time = anchorTime + roundedUpDeltaTime;
-
-      // Stop the current loop (when it gets to the next 4-beat boundary).
-      this.stop(time);
-
-      // Create a new source.
-      const newSource = context.createBufferSource();
-      if (this == source1) {
-        newSource.connect(source1Gain);
-      } else if (this == source2) {
-        newSource.connect(source2Gain);
-      }
-
-      // Assign the buffer to the new source.
-      newSource.buffer = buffer;
-
-      // Start playing exactly on the next 4-beat boundary with looping.
-      newSource.loop = true;
-      newSource.start(time);
-
-      // This new source will replace the existing source.
-      if (this == source1) {
-        source1 = newSource;
-      } else if (this == source2) {
-        source2 = newSource;
-      }
-    }.bind(this), function() {
-      console.log('error decoding file.');
-    } );
-  }.bind(source);
-
-  request.send();
+  // Start playing exactly on the next 4-beat boundary with looping.
+  newSource.loop = true;
+  newSource.start(time);
 };
-
-
-if ( !window.requestAnimationFrame ) {
-  window.requestAnimationFrame = ( function() {
-    return window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame || // comment out if FF4 is slow (it caps framerate at ~30fps: https://bugzilla.mozilla.org/show_bug.cgi?id=630127)
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function(callback, element ) {
-                  window.setTimeout( callback, 1000 / 60 );
-                };
-  } )();
-}
 
 function draw() {
   const canvas = document.getElementById('loop');
@@ -241,12 +132,8 @@ function draw() {
   const width = canvas.width;
   const height = canvas.height;
 
-  // Draw background.
-
   const loopColor = 'rgb(200,150,150)';
 
-
-  // ctx.fillStyle = backgroundColor;
   ctx.clearRect(0, 0, width, height);
 
   // Draw body of knob.
@@ -265,19 +152,20 @@ function draw() {
   ctx.arc(width / 2, height / 2, knobRadius, 0, angle, false);
   ctx.fill();
 
-  window.requestAnimationFrame(draw);
+  requestAnimationFrame(draw);
 }
 
-function init() {
+async function init() {
   // Initialize audio
   context = new AudioContext();
 
-
   // Create post-compressor gain node.
-  postCompressorGain = context.createGain();
+  const postCompressorGain = context.createGain();
   postCompressorGain.gain.value = 1.4;
 
   postCompressorGain.connect(context.destination);
+
+  let compressor;
 
   if (context.createDynamicsCompressor) {
     // Create dynamics compressor to sweeten the overall mix.
@@ -298,10 +186,8 @@ function init() {
   source2Gain = context.createGain();
 
   // Create pre-compressor gain node.
-  preCompressorGain = context.createGain();
+  const preCompressorGain = context.createGain();
   preCompressorGain.gain.value = 0.4;
-
-
   preCompressorGain.connect(compressor);
 
   // Create gain nodes to control the wet (effect) mix levels for left/right
@@ -319,7 +205,7 @@ function init() {
   lowFilter2.Q.value = 5.0; // in decibels
 
   // Create a convolver for a rhythm effect
-  convolver = context.createConvolver();
+  const convolver = context.createConvolver();
 
   // Connect sources to gain and filter nodes.
   source1.connect(source1Gain);
@@ -345,15 +231,33 @@ function init() {
   source1.loop = true;
   source2.loop = true;
 
-  // load reverb
-  // setReverbImpulseResponse('impulse-responses/spatialized3.wav');
-  setReverbImpulseResponse('impulse-responses/filter-rhythm2.wav');
+  // Load initial loop samples and reverb.
+  [source1.buffer, source2.buffer, convolver.buffer] = await Promise.all([
+    fetchAndDecodeAudio('sounds/drum-samples/loops/blueyellow.wav'),
+    fetchAndDecodeAudio('sounds/drum-samples/loops/break29.wav'),
+    fetchAndDecodeAudio('impulse-responses/filter-rhythm2.wav'),
+  ]);
 
-  bufferLoader1 = new BufferLoader(source1,
-      'sounds/drum-samples/loops/blueyellow.wav');
-  bufferLoader2 = new BufferLoader(source2,
-      'sounds/drum-samples/loops/break29.wav');
+  // Get rid of loading animation.
+  const loading = document.getElementById('loading');
+  loading.innerHTML = '';
 
-  bufferLoader1.load();
-  bufferLoader2.load();
+  addSlider('crossfade');
+  addSlider('leftFilter');
+  addSlider('rightFilter');
+  addSlider('leftRhythmEffect');
+  addSlider('rightRhythmEffect');
+  configureSlider('crossfade', 0.5, 0.0, 1.0, crossfadeHandler);
+  configureSlider('leftFilter', 0.99, 0.0, 1.0, leftFilterHandler);
+  configureSlider('rightFilter', 0.99, 0.0, 1.0, rightFilterHandler);
+  configureSlider('leftRhythmEffect', 0.0, 0.0, 1.0, leftRhythmEffectHandler);
+  configureSlider('rightRhythmEffect', 0.0, 0.0, 1.0, rightRhythmEffectHandler);
+
+  const now = context.currentTime;
+  anchorTime = now + 0.040;
+
+  source1.start(anchorTime);
+  source2.start(anchorTime);
+
+  draw();
 }
