39e9608e484bcf3de4af4e0952650e37ac602442
pr3cast <pr3castdragon@gmail.com>
2022-07-29 17:55:23 -0400

    update comments, refactor viz data input

--- src/audio-worklet/migration/spn-recorder/app.js
+++ src/audio-worklet/migration/spn-recorder/app.js
@@ -1,8 +1,7 @@
-/**
- * @license Copyright (c) Google. MIT License.
- * @fileOverview Provides functionality for
- * the ScriptProcessorNode Recording Demo.
- */
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 'use strict';
 
 import createLinkFromAudioBuffer from './exporter.mjs';
@@ -11,27 +10,26 @@ const context = new AudioContext();
 
 // Arbitrary buffer size, not specific for a reason
 const BUFFER_SIZE = 256;
+let recordingLength = 0;
 let isRecording = false;
 let isMonitoring = false;
 let visualizationEnabled = true;
-let currentSample = [];
-let recordingLength = 0;
 
-// Wait for user interaction to initialize audio, as per specification
+// Wait for user interaction to initialize audio, as per specification.
 document.addEventListener('click', (element) => {
   init();
   document.querySelector('#click-to-start').remove();
 }, {once: true});
 
 /**
- * Defines overall audio chain and initializes all functionality
+ * Defines overall audio chain and initializes all functionality.
  */
 async function init() {
   if (context.state === 'suspended') {
     await context.resume();
   }
 
-  // Get user's microphone and connect it to the AudioContext
+  // Get user's microphone and connect it to the AudioContext.
   const micStream = await navigator.mediaDevices.getUserMedia({
     audio: {
       echoCancellation: false,
@@ -43,14 +41,17 @@ async function init() {
 
   const micSourceNode = context.createMediaStreamSource(micStream);
 
-  // Prepare buffer for recording
+  // Prepare buffer for recording.
   const recordBuffer = context.createBuffer(
       2,
-      // 10 seconds seems reasonable for demo purposes
+      // 10 seconds seems reasonable for demo purposes.
       context.sampleRate * 10,
       context.sampleRate,
   );
-  const spNode = setupScriptProcessor(recordBuffer);
+
+  // Obtain samples passthrough function for visualizers
+  const passSampleToVisualizers = setupVisualizers();
+  const spNode = setupScriptProcessor(recordBuffer, passSampleToVisualizers);
 
   const monitorNode = context.createGain();
   const inputGain = context.createGain();
@@ -58,7 +59,6 @@ async function init() {
 
   setupMonitor(monitorNode);
   setupRecording(recordBuffer);
-  setupVisualizers();
 
   micSourceNode
       .connect(inputGain)
@@ -71,35 +71,39 @@ async function init() {
 /**
  * Creates ScriptProcessor to record and track microphone audio.
  * @param {AudioBuffer} recordBuffer
+ * @param {function} passSampleToVisualizers
+ *    Function to pass current samples to visualizers.
  * @return {ScriptProcessorNode} ScriptProcessorNode to pass audio into.
  */
-function setupScriptProcessor(recordBuffer) {
+function setupScriptProcessor(recordBuffer, passSampleToVisualizers) {
   const processor = context.createScriptProcessor(BUFFER_SIZE);
-  currentSample = new Array(recordBuffer.numberOfChannels).fill([]);
+  const currentSamples = new Array(recordBuffer.numberOfChannels).fill([]);
 
-  // Main SPN callback. Handles recording data and tracking recording length
+  // Main SPN callback. Handles recording data and tracking recording length.
   processor.onaudioprocess = function(event) {
-    // Perform logic on all mic channels, regardless of setup
-    for (let channel = 0; channel < currentSample.length; channel++) {
+    // Perform logic on all input channels, regardless of setup.
+    for (let channel = 0; channel < currentSamples.length; channel++) {
       const inputData = event.inputBuffer.getChannelData(channel);
 
-      // Provide current sample to visualizers
-      currentSample[channel] = inputData;
+      // Provide current sample to visualizers.
+      currentSamples[channel] = inputData;
 
-      // While recording, feed data to recording buffer at the proper time
+      // While recording, feed data to recording buffer at the proper time.
       if (isRecording) {
         recordBuffer
-            .copyToChannel(currentSample[channel], channel, recordingLength);
+            .copyToChannel(currentSamples[channel], channel, recordingLength);
       }
 
-      // Pass audio data through to next node
+      // Pass audio data through to next node.
       event.outputBuffer.copyToChannel(inputData, channel, 0);
     }
 
-    // Update tracked recording length
+    // Update tracked recording length.
     if (isRecording) {
       recordingLength += BUFFER_SIZE;
     }
+
+    passSampleToVisualizers(currentSamples);
   };
 
   return processor;
@@ -116,14 +120,14 @@ function setupRecording(recordBuffer) {
   const downloadButton = document.querySelector('#download');
 
   async function prepareClip() {
-    // Create recording file URL for playback and download
+    // Create recording file URL for playback and download.
     const wavUrl = createLinkFromAudioBuffer(recordBuffer, true);
 
     player.src = wavUrl;
     downloadButton.src = wavUrl;
     downloadButton.download = 'recording.wav';
 
-    // Display current recording length
+    // Display current recording length.
     document.querySelector('#data-len').innerHTML =
         Math.round(recordingLength / recordBuffer.sampleRate * 100)/100;
   }
@@ -131,7 +135,7 @@ function setupRecording(recordBuffer) {
   recordButton.addEventListener('click', (e) => {
     isRecording = !isRecording;
 
-    // When recording is paused, process clip
+    // When recording is paused, process clip.
     if (!isRecording) {
       prepareClip();
     }
@@ -145,7 +149,7 @@ function setupRecording(recordBuffer) {
  * @param {GainNode} monitorNode Gain node to adjust for monitor gain.
  */
 function setupMonitor(monitorNode) {
-  // Leave audio volume at zero by default
+  // Leave audio volume at zero by default.
   monitorNode.gain.value = 0;
 
   const monitorButton = document.querySelector('#monitor');
@@ -155,7 +159,7 @@ function setupMonitor(monitorNode) {
     isMonitoring = !isMonitoring;
     const newVal = isMonitoring ? 1 : 0;
 
-    // Set gain to quickly but smoothly slide to new value
+    // Set gain to quickly but smoothly slide to new value.
     monitorNode.gain.setTargetAtTime(newVal, context.currentTime, 0.01);
 
     monitorText.innerHTML = isMonitoring ? 'off' : 'on';
@@ -164,10 +168,22 @@ function setupMonitor(monitorNode) {
 
 /**
  * Sets up and handles calculations and rendering for all visualizers.
+ * @return {function} Function to set current input samples for visualization.
  */
 function setupVisualizers() {
   const drawLiveGain = setupLiveGainVis();
   const drawRecordingGain = setupRecordingGainVis();
+  let currentSamples;
+  let firstSamplesReceived = false;
+
+  const setCurrentSamples = (newSamples) => {
+    currentSamples = newSamples;
+
+    if (!firstSamplesReceived) {
+      draw();
+      firstSamplesReceived = true;
+    }
+  };
 
   const visToggle = document.querySelector('#viz-toggle');
   visToggle.addEventListener('click', (e) => {
@@ -177,18 +193,18 @@ function setupVisualizers() {
   });
 
   function draw() {
-    if (visualizationEnabled) {
+    if (visualizationEnabled && currentSamples) {
       // Calculate current sample's average gain for visualizers to draw with.
-      // We only need to calculate this value once per render frame
+      // We only need to calculate this value once per render frame.
       let currentSampleGain = 0;
 
-      for (let i = 0; i < currentSample.length; i++) {
-        for (let j = 0; j < currentSample[i].length; j++) {
-          currentSampleGain += currentSample[i][j];
+      for (let i = 0; i < currentSamples.length; i++) {
+        for (let j = 0; j < currentSamples[i].length; j++) {
+          currentSampleGain += currentSamples[i][j];
         }
       }
 
-      currentSampleGain /= (currentSample.length *currentSample[0].length);
+      currentSampleGain /= (currentSamples.length *currentSamples[0].length);
 
       drawLiveGain(currentSampleGain);
 
@@ -198,11 +214,12 @@ function setupVisualizers() {
     }
 
     // Request render frame regardless.
-    // If visualizers are disabled, function can still wait for enable
+    // If visualizers are disabled, function can still wait for enable.
     requestAnimationFrame(draw);
   }
 
-  draw();
+
+  return setCurrentSamples;
 }
 
 /**
@@ -219,20 +236,20 @@ const setupLiveGainVis = () => {
   const drawStart = width-1;
 
   function draw(currentSampleGain) {
-    // Determine center and height
+    // Determine center and height.
     const centerY = ((1 - currentSampleGain) * height) / 2;
     const gainHeight = currentSampleGain * height;
 
-    // Draw gain bar
+    // Draw gain bar.
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(drawStart, centerY, 1, gainHeight);
 
-    // Copy visualizer left
+    // Copy visualizer left.
     canvasContext.globalCompositeOperation = 'copy';
     canvasContext.drawImage(canvas, -1, 0);
 
-    // Return to original state, where new visuals
-    // are drawn without clearing the canvas
+    // Return to original state, where new visuals.
+    // are drawn without clearing the canvas.
     canvasContext.globalCompositeOperation = 'source-over';
   }
 
@@ -259,28 +276,28 @@ function setupRecordingGainVis() {
     const centerY = ((1 - currentSampleGain) * height) / 2;
     const gainHeight = currentSampleGain * height;
 
-    // Clear current Y-axis
+    // Clear current Y-axis.
     canvasContext.clearRect(currX, 0, 1, height);
 
-    // Draw recording bar 1 ahead
+    // Draw recording bar 1 ahead.
     canvasContext.fillStyle = 'red';
     canvasContext.fillRect(currX+1, 0, 1, height);
 
-    // Draw current gain
+    // Draw current gain.
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(currX, centerY, 1, gainHeight);
 
     if (currX < width - 2) {
-      // Keep drawing new waveforms rightwards until canvas is full
+      // Keep drawing new waveforms rightwards until canvas is full.
       currX++;
     } else {
       // If the waveform fills the canvas,
-      // move it by one pixel to the left to make room
+      // move it by one pixel to the left to make room.
       canvasContext.globalCompositeOperation = 'copy';
       canvasContext.drawImage(canvas, -1, 0);
 
       // Return to original state, where new visuals
-      // are drawn without clearing the canvas
+      // are drawn without clearing the canvas.
       canvasContext.globalCompositeOperation = 'source-over';
     }
   }
