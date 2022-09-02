782f296823260e5d33f4d3c64c2db57497b10d20
hoch <hongchan.choi@gmail.com>
2022-09-02 14:22:12 +0000

    Update gh-pages to output generated at 97014cc

---
+++ audio-worklet/free-queue/examples/simple-passthrough/basic-processor.js
@@ -0,0 +1,50 @@
+import FreeQueue from "../../src/free-queue.js";
+import { FRAME_SIZE, RENDER_QUANTUM } from "./constants.js";
+
+/**
+ * A simple AudioWorkletProcessor node.
+ *
+ * @class BasicProcessor
+ * @extends AudioWorkletProcessor
+ */
+class BasicProcessor extends AudioWorkletProcessor {
+  /**
+   * Constructor to initialize, input and output FreeQueue instances
+   * and atomicState to synchronise Worker with AudioWorklet
+   * @param {Object} options AudioWorkletProcessor options
+   *    to initialize inputQueue, outputQueue and atomicState
+   */
+  constructor(options) {
+    super();
+
+    this.inputQueue = options.processorOptions.inputQueue;
+    this.outputQueue = options.processorOptions.outputQueue;
+    this.atomicState = options.processorOptions.atomicState;
+    Object.setPrototypeOf(this.inputQueue, FreeQueue.prototype);
+    Object.setPrototypeOf(this.outputQueue, FreeQueue.prototype);
+
+  }
+
+  process(inputs, outputs) {
+    const input = inputs[0];
+    const output = outputs[0];
+    
+    // Push data from input into inputQueue.
+    this.inputQueue.push(input, RENDER_QUANTUM);
+    
+    // Try to pull data out of outputQueue and store it in output.
+    const didPull = this.outputQueue.pull(output, RENDER_QUANTUM);
+    if (!didPull) {
+      console.log("failed to pull.");
+    }
+    
+    // Wake up worker to process a frame of data.
+    if (this.inputQueue.isFrameAvailable(FRAME_SIZE)) {
+      Atomics.notify(this.atomicState, 0, 1);
+    }
+    
+    return true;
+  }
+}
+
+registerProcessor('basic-processor', BasicProcessor);
