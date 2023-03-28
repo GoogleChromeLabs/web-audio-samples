2ecf52cd49caf5a1a8022adb69fd93dc2636ce09
Sina Firoozabadi <sinafirooz@google.com>
2023-03-28 16:15:05 -0700

    Move creating worker.js worker outside the event listener
    This somehow fixes the error when creating the worker.

--- src/experiments/webgpuaudio/main.js
+++ src/experiments/webgpuaudio/main.js
@@ -70,12 +70,13 @@ const toggleButtonClickHandler = async () => {
   }
 };
 
+// Create a WebWorker for Audio Processing.
+const worker = new Worker('worker.js', {type: 'module'});
+worker.onerror = function () {console.log("Error creating the worker.js worker!");};
+
 window.addEventListener('load', () => {
   toggleButton = document.getElementById('toggle-audio');
   toggleButton.onclick = toggleButtonClickHandler;
-
-  // Create a WebWorker for Audio Processing.
-  const worker = new Worker('worker.js', {type: 'module'});
 
   // Send FreeQueue instance and atomic state to worker.
   worker.postMessage({
