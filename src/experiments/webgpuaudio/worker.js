8295d769b43ba593b00109fbc3af362db0b5abc7
Hongchan Choi <hongchan.choi@gmail.com>
2023-03-30 11:58:48 -0700

    Refactoring worker.js and integrate ir-helper.js

--- src/experiments/webgpuaudio/worker.js
+++ src/experiments/webgpuaudio/worker.js
@@ -3,43 +3,58 @@ import GPUProcessor from "./gpu-processor.js";
 import IRHelper from "./ir-helper.js"
 import { FRAME_SIZE } from "./constants.js";
 
-/**
- * Worker message event handler.
- * This will initialize worker with FreeQueue instance and set loop for audio
- * processing. 
- */
-self.onmessage = async (msg) => {
-  if (msg.data.type === "init") {
-    let { inputQueue, outputQueue, atomicState } = msg.data.data;
-    Object.setPrototypeOf(inputQueue, FreeQueue.prototype);
-    Object.setPrototypeOf(outputQueue, FreeQueue.prototype);
+// Harmful globals
+let inputQueue = null;
+let outputQueue = null;
+let atomicState = null;
+let gpuProcessor = null;
+let inputBuffer = null;
 
-    const gpuProcessor = new GPUProcessor();
-    await gpuProcessor.init();
-    
-    // buffer for storing data pulled out from queue.
-    const input = new Float32Array(FRAME_SIZE);
-    // loop for processing data.
-    while (Atomics.wait(atomicState, 0, 0) === 'ok') {      
-      // pull data out from inputQueue.
-      const start = performance.now();
-      const didPull = inputQueue.pull([input], FRAME_SIZE);
-      if (didPull) {
-        // If pulling data out was successfull, process it and push it to
-        // outputQueue.
+// This will initialize worker with FreeQueue instance and set loop for audio
+// processing.
+const initialize = async (messageDataFromMainThread) => {
+  inputQueue = messageDataFromMainThread.inputQueue;
+  outputQueue = messageDataFromMainThread.outputQueue;
+  atomicState = messageDataFromMainThread.atomicState;
+  Object.setPrototypeOf(inputQueue, FreeQueue.prototype);
+  Object.setPrototypeOf(outputQueue, FreeQueue.prototype);
 
-        const output_old = input.map(sample => 0.1 * sample);
-        const end_old = performance.now();
-        console.log("Time for output using FIFO Queue "+(end_old - start)+" ms");
+  // A local buffer to store data pulled out from `inputQueue`.
+  inputBuffer = new Float32Array(FRAME_SIZE);
 
-        const start_new = performance.now();
-        const output = await gpuProcessor.processInputAndReturn(input);
-        const end_new = performance.now();
-        console.log("Time for GPU Processing "+(end_new - start_new)+" ms.");
-        outputQueue.push([output], FRAME_SIZE);
-      } 
+  // Create an instance of GPUProcessor and provide an IR array.
+  gpuProcessor = new GPUProcessor();
+  gpuProcessor.setIRArray(IRHelper.createTestIR());
+  await gpuProcessor.initialize();
 
-      Atomics.store(atomicState, 0, 0);
-    }
+  console.log('[worker.js] initialize()');
+};
+
+const process = async () => {
+  const processStart = performance.now();
+  
+  if (!inputQueue.pull([inputBuffer], FRAME_SIZE)) {
+    console.error('[worker.js] Pulling from inputQueue failed.');
+    return;
+  }
+  const output = await gpuProcessor.processInputAndReturn(inputBuffer);
+  outputQueue.push([output], FRAME_SIZE);
+
+  const processEnd = performance.now();
+  console.log(`[worker.js] process() took ${processEnd - processStart}ms.`);
+};
+
+self.onmessage = async (message) => {
+  if (message.data.type !== 'init') {
+    console.error(`[worker.js] Invalid message type: ${message.data.type}`);
+    return;
+  }
+
+  await initialize(message.data.data);
+  while (Atomics.wait(atomicState, 0, 0) === 'ok') {
+    await process();
+    Atomics.store(atomicState, 0, 0);
   }
 };
+
+console.log('[worker.js] loaded.');
