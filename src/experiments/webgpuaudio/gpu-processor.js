5b35aa444bed3369d9b4a5b88be5553049d6ba44
Dibyajyoti Pal <dibyapal@google.com>
2023-03-29 17:54:52 -0700

    Tru out WGSL

--- src/experiments/webgpuaudio/gpu-processor.js
+++ src/experiments/webgpuaudio/gpu-processor.js
@@ -1,5 +1,7 @@
 import { FRAME_SIZE } from "./constants.js";
 
+const WORKGROUP_SIZE = 4;
+
 class GPUProcessor {
   constructor() {}
 
@@ -14,20 +16,30 @@ class GPUProcessor {
   }
 
   processInputAndReturn = async(input) => {
+    // --------------WRITE TO INPUT BUFFER-------------
     // Get a GPU buffer in a mapped state and an arrayBuffer for writing.
     const gpuWriteBuffer = this.device.createBuffer({
         mappedAtCreation: true,
         size: FRAME_SIZE * Float32Array.BYTES_PER_ELEMENT,
-        usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.COPY_SRC
+        usage: GPUBufferUsage.STORAGE
     });
 
-    // Get a GPU buffer for reading in an unmapped state.
+    // Write data to the GPUWriteBuffer first and unmap so that it can be used later for copying.
+    const arrayBuffer = gpuWriteBuffer.getMappedRange();
+    new Float32Array(arrayBuffer).set(input);
+    gpuWriteBuffer.unmap();
+
+    // --------------COMPUTATION STAGE-----------------
+    // Create a compute buffer that is used for storing computed data.
     const gpuComputeBuffer = this.device.createBuffer({
-        mappedAtCreation: false,
         size: FRAME_SIZE * Float32Array.BYTES_PER_ELEMENT,
         usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
     });
+    gpuComputeBuffer.unmap();
 
+
+    // Compute shader code defining the group, bindings and the WORKGROUP_SIZE
+    // needed to parallelize the work.
     const computeModule = this.device.createShaderModule({
         code: `
           @group(0) @binding(0)
@@ -36,59 +48,55 @@ class GPUProcessor {
           @group(0) @binding(1)
           var<storage, read_write> output: array<f32>;
 
-          @compute @workgroup_size(1)
-          fn computeMain(@builtin(global_invocation_id) global_id : vec3<u32>) {
+          @compute @workgroup_size(${WORKGROUP_SIZE})
+          fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
             output[global_id.x] = input[global_id.x] * 0.1;
           }
         `,
     });
 
+    // Create a compute pipeline with the layout and compute defined.
     const computePipeline = this.device.createComputePipeline({
-        layout: 'auto',
+        layout : "auto",
         compute: {
           module: computeModule,
-          entryPoint: 'computeMain',
+          entryPoint: "main",
         }
     });
 
     const bindGroup = this.device.createBindGroup({
-        layout: computePipeline.getBindGroupLayout(0),
-        entries: [{
-        binding: 0,
-        resource: { buffer: gpuWriteBuffer }
-        }, {
-        binding: 1,
-        resource: { buffer: gpuComputeBuffer }
-        }]
-    });
+        layout: computePipeline.getBindGroupLayout(/*index=*/0),
+        entries: [
+          {
+            binding: 0,
+            resource: {
+              buffer: gpuWriteBuffer
+            }
+          },
+          {
+            binding: 1,
+            resource: {
+              buffer: gpuComputeBuffer
+            }
+          }
+        ]
+      });
 
-
-    // const arrayBuffer = gpuWriteBuffer.getMappedRange();
-
-    // // Write bytes to buffer.
-    // new Float32Array(arrayBuffer).set(input);
-
-    // // Unmap buffer so that it can be used later for copy.
-    // gpuWriteBuffer.unmap();
-
-    this.device.queue.writeBuffer(gpuWriteBuffer, 0, input);
-
-    const encoder = this.device.createCommandEncoder();
-    const pass = encoder.beginComputePass();
-    pass.setPipeline(computePipeline);
-    pass.setBindGroup(0, bindGroup);
-    pass.dispatchWorkgroups(FRAME_SIZE);
-    pass.end();
-    this.device.queue.submit([ encoder.finish() ]);
+    // Create a commandEncoder that is responsible for running the commands inside the GPU.
+    const commandEncoder = this.device.createCommandEncoder();
+    const computePass = commandEncoder.beginComputePass();
+    computePass.setPipeline(computePipeline);
+    computePass.setBindGroup(0, bindGroup);
+    computePass.dispatchWorkgroups(FRAME_SIZE / WORKGROUP_SIZE);
+    computePass.end();
 
     const gpuReadBuffer = this.device.createBuffer({
         size: FRAME_SIZE * Float32Array.BYTES_PER_ELEMENT,
         usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
     });
       
-    // Encode commands for copying buffer to buffer.
-    const copyEncoder = this.device.createCommandEncoder();
-    copyEncoder.copyBufferToBuffer(
+    // Encode commands for copying from the compute to the read buffer.
+    commandEncoder.copyBufferToBuffer(
         gpuComputeBuffer /* source buffer */,
         0 /* source offset */,
         gpuReadBuffer /* destination buffer */,
@@ -97,15 +105,12 @@ class GPUProcessor {
     );
 
     // Submit copy commands.
-    const copyCommands = copyEncoder.finish();
-    this.device.queue.submit([copyCommands]);
+    const gpuCommandsEnd = commandEncoder.finish();
+    this.device.queue.submit([ gpuCommandsEnd ]);
 
-    // Read buffer.
+    // --------------READ STAGE-----------------
     await gpuReadBuffer.mapAsync(GPUMapMode.READ);
     const copyArrayBuffer = gpuReadBuffer.getMappedRange();
-    console.log(new Float32Array(copyArrayBuffer));
-    // .map(sample => 0.1 * sample)
-
     return new Float32Array(copyArrayBuffer);
   }
 };
