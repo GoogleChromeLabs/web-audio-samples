1a6e311238cfe8c86cd5ed1f7ecb43769fb1d14b
TenghuiZhang <32180291+TenghuiZhang@users.noreply.github.com>
2023-06-09 19:42:07 +0000

    Update gh-pages to output generated at 0d979a5

--- audio-worklet/migration/worklet-recorder/app.js
+++ audio-worklet/migration/worklet-recorder/app.js
@@ -16,8 +16,10 @@ const RecorderStates = {
 
 const context = new AudioContext();
 
-// Make the visulization more clear to the users
-const WAVEFROM_SCALE_FACTOR = 5;
+// Make the visulization clearer to the users
+const SCALE_FACTOR = 10;
+// Make the visulization of vu meter more clear to the users
+const MAX_GAIN = 1;
 let recordingState = RecorderStates.UNINITIALIZED;
 
 let recordButton = document.querySelector('#record');
@@ -26,6 +28,8 @@ let stopButton = document.querySelector('#stop');
 let player = document.querySelector('#player');
 let downloadLink = document.querySelector('#download-link');
 let downloadButton = document.querySelector('#download-button');
+
+recordButton.disabled = false;
 
 // Wait for user interaction to initialize audio, as per specification.
 recordButton.disabled = false;
@@ -204,7 +208,8 @@ function setupVisualizers() {
   function draw() {
     if (recordingState === RecorderStates.RECORDING) {
       const recordGain = gain;
-      drawRecordingGain(recordGain * WAVEFROM_SCALE_FACTOR);
+      drawVUMeter(recordGain);
+      drawRecordingGain(recordGain);
     }
 
     // Request render frame regardless.
@@ -231,11 +236,16 @@ function setupRecordingGainVis() {
 
   let currentX = 0;
   let previousY = height / 2;
-  // Adjust the amplitude value to increase the size of the waveform
-  const amplitude = height * 2;
 
   function draw(currentSampleGain) {
-    const centerY = height / 2 - currentSampleGain * amplitude;
+    // This formula is design based on this logic:
+    // Middle line of canvas: height / 2
+    // Current sound wave gain value range is -1 to 1
+    // We want use current gain value divide by gain value range and
+    // time half of canvas height, therefore, we can get the
+    // accurate wave size.
+    // At the end, use scale_factor to make is clearer for users
+    const currentY = height / 2 * (1 - currentSampleGain * SCALE_FACTOR);
 
     canvasContext.clearRect(currentX, 0, 1, height);
 
@@ -244,12 +254,12 @@ function setupRecordingGainVis() {
 
     canvasContext.beginPath();
     canvasContext.moveTo(currentX, previousY);
-    canvasContext.lineTo(currentX + 1, centerY);
+    canvasContext.lineTo(currentX + 1, currentY);
     canvasContext.strokeStyle = 'black';
     canvasContext.lineWidth = 0.8;
     canvasContext.stroke();
 
-    previousY = centerY;
+    previousY = currentY;
 
     if (currentX < width - 2) {
       // Keep drawing new waveforms rightwards until the canvas is full.
@@ -297,3 +307,33 @@ const createRecord = (recordingProperties, recordingLength, sampleRate,
       `recording-${new Date().getMilliseconds().toString()}.wav`;
   downloadButton.disabled = false;
 };
+
+function drawVUMeter(volume) {
+  var canvas = document.getElementById('vu-meter');
+  var ctx = canvas.getContext('2d');
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  var meterHeight = canvas.height *
+      (volume / MAX_GAIN) * SCALE_FACTOR;
+  
+  ctx.fillStyle = '#f00';
+  ctx.fillRect(0, canvas.height - meterHeight, canvas.width, meterHeight);
+  
+  ctx.strokeStyle = '#fff';
+  ctx.lineWidth = 1;
+  ctx.globalAlpha = 0.3;
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height * 0.2);
+  ctx.lineTo(canvas.width, canvas.height * 0.2);
+  ctx.moveTo(0, canvas.height * 0.4);
+  ctx.lineTo(canvas.width, canvas.height * 0.4);
+  ctx.moveTo(0, canvas.height * 0.6);
+  ctx.lineTo(canvas.width, canvas.height * 0.6);
+  ctx.moveTo(0, canvas.height * 0.8);
+  ctx.lineTo(canvas.width, canvas.height * 0.8);
+  ctx.stroke();
+}
