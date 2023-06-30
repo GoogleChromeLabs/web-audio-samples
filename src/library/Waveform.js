1ccfdec080c7c84a2637bdc0c4148b5c99f17a1c
Tenghui Zhang <32180291+TenghuiZhang@users.noreply.github.com>
2023-06-30 12:00:47 -0700

    format

--- src/library/Waveform.js
+++ src/library/Waveform.js
@@ -1,20 +1,20 @@
 /**
- * This Waveform class implements a Waveform UI. To initialize it, you
+ * This Waveform class implements a waveform UI. To initialize it, you
  * need to provide the ID of the canvas element where the waveform
- * will be rendered, and the fftSize value for the analyserNode.  The
- * fftSize value defines the window size in samples that is used when
- * performing a Fast Fourier Transform (FFT) to get frequency domain
- * data in the AnalyserNode. When the waveform is created, it requires
- * an AnalyserNode for real-time audio input data. This class only
- * requires a canvas element, and there is no additional dependency.
+ * will be rendered and the fftSize value for the analyserNode. The
+ * fftSize value defines the window size in samples used when
+ * performing a Fast Fourier Transform (FFT) to obtain frequency
+ * domain data in the AnalyserNode. When the waveform is created, it
+ * requires an AnalyserNode for real-time audio input data. This class
+ * only requires a canvas element, and there are no additional
+ * dependencies.
  * @class
  */
-
 class Waveform {
 
   /**
    * @constructor Constructs a Waveform object for rendering waveform
-   * visuals.  The width and height of the waveform canvas are
+   * visuals. The width and height of the waveform canvas are
    * initialized in the constructor and cannot be changed after
    * initialization.
    * @param {String} canvasId A canvas element ID where the
@@ -26,31 +26,31 @@ class Waveform {
    * AnalyserNode.
    */
   constructor(canvasId, analyserNode, fftSize) {
-    /** @private @const {!HTMLCanvasElement} Selected waveform canvas
+    /** @private @const {!canvas} Selected waveform canvas
      * element */
     this.canvas_ = document.querySelector(canvasId);
-    /** @private @const {!CanvasRenderingContext2D} Canvas context */
+    /** @private @const {!canvasContext} Canvas context */
     this.canvasContext_ = this.canvas_.getContext('2d');
-    /** @private @const {!number} Selected waveform canvas width */
+    /** @private @const {!width} Selected waveform canvas width */
     this.width_ = this.canvas_.width;
-    /** @private @const {!number} Selected waveform canvas height */
+    /** @private @const {!height} Selected waveform canvas height */
     this.height_ = this.canvas_.height;
-    /** @private {!number} Current X axis in the waveform */
+    /** @private {!currentX} Current X axis in the waveform */
     this.currentX_ = 0;
-    /** @private {!number} Previous Y axis in the waveform */
+    /** @private {!previousY} Previous Y axis in the waveform */
     this.previousY_ = this.height_ / 2;
-    /** @private @const {!AnalyserNode} AnalyserNode that will be used
-     * for visualization. */
+    /** @private @const {!analyser} AnalyserNode that will be used
+     * for visualization */
     this.analyser_ = analyserNode;
     this.analyser_.fftSize = fftSize;
-    /** @private @const {!Float32Array} The array that the time domain
+    /** @private @const {!dataArray} The array that the time domain
      * data will be copied to */
     this.dataArray_ = new Float32Array(this.analyser_.frequencyBinCount);
   }
   
   /**
-   * Render the Waveform on the canvas.  This function is called
-   * periodically to update the Waveform display.  This class doesn't
+   * Render the waveform on the canvas. This function is called
+   * periodically to update the waveform display. This class doesn't
    * have a timer or a render loop; it requires an external driver for
    * the actual rendering.
    */
@@ -78,11 +78,11 @@ class Waveform {
     this.previousY_ = currentY;
 
     if (this.currentX_ < this.width_ - 2) {
-        this.currentX_ += 2;
+      this.currentX_ += 2;
     } else {
-        this.canvasContext_.globalCompositeOperation = 'copy';
-        this.canvasContext_.drawImage(this.canvas_, -2, 0);
-        this.canvasContext_.globalCompositeOperation = 'source-over';
+      this.canvasContext_.globalCompositeOperation = 'copy';
+      this.canvasContext_.drawImage(this.canvas_, -2, 0);
+      this.canvasContext_.globalCompositeOperation = 'source-over';
     }
   }
 }
