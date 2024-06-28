f2e77aa07f254a521b5b8a93311180622c315146
Kizjkre <Kizjkre@users.noreply.github.com>
2024-06-28 00:55:49 +0000

    Update gh-pages to output generated at 32de3b3

--- tests/live-suite/scripts/main.js
+++
@@ -1,6 +0,0 @@
-import convert from './converter.js';
-
-const files = ['realtime-sine.html', 'offline-sine.html', 'audioworklet-addmodule-resolution.html']
-  .map(file => `./../playwright/pages/${ file }`);
-
-convert(files);
