4e8236e9baddf4a5f066378a647727f521d06a0a
Tristan <24882191+Kizjkre@users.noreply.github.com>
2024-07-30 16:02:19 -0700

    Add run all button and total number of tests passed in live suite
    Co-authored-by: terryzfeng <terryzfeng@users.noreply.github.com>

--- src/tests/playwright/pages/live-suite/scripts/main.js
+++ src/tests/playwright/pages/live-suite/scripts/main.js
@@ -7,8 +7,21 @@ import {convertTestFiles} from './test-file-converter.js';
 // Flag for live test suite environment
 window._isTestSuiteMode = true;
 
+const testEnded = () =>
+    new Promise((resolve) => window._webAudioTestEnded = resolve);
+document.querySelector('#run-all').addEventListener('click', async () => {
+  const buttons = document.querySelectorAll('button[data-type=test]');
+  for (const button of buttons) {
+    button.click();
+    await testEnded();
+  }
+});
+
 (async () => {
   const tests = await (await fetch('tests.json')).json();
+
+  document.querySelector('#total').innerText =
+      Object.values(tests).reduce((sum, group) => sum + group.length, 0);
 
   convertTestFiles([
     ...tests.benchmark.map((test) => test.path),
