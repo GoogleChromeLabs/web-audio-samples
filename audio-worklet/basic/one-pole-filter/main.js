47dc60df4980770ac8d37965f38d1ed8234497f2
hoch <hongchan.choi@gmail.com>
2024-08-20 19:52:34 +0000

    Update gh-pages to output generated at 15af0c2

--- audio-worklet/basic/one-pole-filter/main.js
+++ audio-worklet/basic/one-pole-filter/main.js
@@ -3,20 +3,35 @@
 // found in the LICENSE file.
 
 const audioContext = new AudioContext();
+let isModuleLoaded = false;
+let isPlaying = false;
+let isGraphReady = false;
+let oscillatorNode = null;
+let filterNode = null;
 
-const startAudio = async (context) => {
-  await context.audioWorklet.addModule('one-pole-processor.js');
-  const oscillator = new OscillatorNode(context);
-  const filter = new AudioWorkletNode(context, 'one-pole-processor');
-  const frequencyParam = filter.parameters.get('frequency');
+const loadGraph = (context) => {
+  oscillatorNode = new OscillatorNode(context);
+  filterNode = new AudioWorkletNode(context, 'one-pole-processor');
+  const frequencyParam = filterNode.parameters.get('frequency');
 
-  oscillator.connect(filter).connect(context.destination);
-  oscillator.start();
+  oscillatorNode.connect(filterNode).connect(context.destination);
+  oscillatorNode.start();
 
   frequencyParam
       .setValueAtTime(0.01, 0)
       .exponentialRampToValueAtTime(context.sampleRate * 0.5, 4.0)
       .exponentialRampToValueAtTime(0.01, 8.0);
+}
+
+const startAudio = async (context) => {
+  if(!isModuleLoaded) {
+    await context.audioWorklet.addModule('one-pole-processor.js');
+    isModuleLoaded = true;
+  }
+  if(!isGraphReady) {
+    loadGraph(audioContext);
+    isGraphReady = true;
+  }
 };
 
 // A simplem onLoad handler. It also handles user gesture to unlock the audio
@@ -24,10 +39,19 @@ const startAudio = async (context) => {
 window.addEventListener('load', async () => {
   const buttonEl = document.getElementById('button-start');
   buttonEl.disabled = false;
+
   buttonEl.addEventListener('click', async () => {
-    await startAudio(audioContext);
-    audioContext.resume();
-    buttonEl.disabled = true;
-    buttonEl.textContent = 'Playing...';
-  }, false);
+    if (!isPlaying) {
+      await startAudio(audioContext);
+      isPlaying = true;
+      buttonEl.textContent = 'Playing...';
+      buttonEl.classList.remove('start-button');
+      audioContext.resume();
+    } else {
+      audioContext.suspend();
+      isPlaying = false;
+      buttonEl.textContent = 'START';
+      buttonEl.classList.add('start-button');
+    }
+  });
 });
