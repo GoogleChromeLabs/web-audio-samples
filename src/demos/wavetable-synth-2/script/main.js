dc476a81af4d9cbf1f73b82c16cab407c18de100
Hongchan Choi <hongchan.choi@gmail.com>
2025-04-16 10:54:20 -0700

    knob and toggle completed

--- src/demos/wavetable-synth-2/script/main.js
+++ src/demos/wavetable-synth-2/script/main.js
@@ -1,8 +1,30 @@
-import { LEDToggleSwitch } from "../ui-components/LEDToggleSwitch.js";
+import { KnobSimple } from "../ui-components/KnobSimple.js";
+import { ToggleSimple } from "../ui-components/ToggleSimple.js";
 
-const ledToggleSwitch = document.getElementById('ledToggleSwitch');
-const statusSpan = document.getElementById('statusSpan');
+const toggle1 = document.querySelector('#toggle-1');
+const toggle2 = document.querySelector('#toggle-2');
+const knob1 = document.querySelector('#knob-1');
+const knob2 = document.querySelector('#knob-2');
+const knob3 = document.querySelector('#knob-3');
 
-ledToggleSwitch.addEventListener('togglechange', (event) => {
-  statusSpan.textContent = event.detail.isActive ? 'ON' : 'OFF';
+console.log(toggle1);
+console.log(toggle2);
+console.log(knob1);
+console.log(knob2);
+console.log(knob3);
+
+toggle1.addEventListener('change', (event) => {
+  console.log('Toggle 1 state changed:', event.detail.state);
+});
+
+toggle2.addEventListener('change', (event) => {
+  console.log('Toggle 1 state changed:', event.detail.state);
+});
+
+knob1.addEventListener('input', (event) => {
+  console.log('Knob 1 value changed:', event.detail.value);
+});
+
+knob1.addEventListener('change', (event) => {
+  console.log('Knob 1 value changed:', event.detail.value);
 });
