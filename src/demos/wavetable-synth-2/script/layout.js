4ca440f7fcf8dd18be24bebbd663b666020863ac
Michael Wilson <mjwilson@chromium.org>
2025-04-18 14:12:28 -0700

    Adjusting layout

--- src/demos/wavetable-synth-2/script/layout.js
+++ src/demos/wavetable-synth-2/script/layout.js
@@ -13,41 +13,42 @@ const handleUIEvent = (event) => {
 };
 
 const initialize = async () => {
-  const toggle1 = document.querySelector('#toggle-1');
-  const toggle2 = document.querySelector('#toggle-2');
-  toggle1.addEventListener('change', handleUIEvent);
-  toggle2.addEventListener('change', handleUIEvent);
+  // const toggle1 = document.querySelector('#toggle-1');
+  // const toggle2 = document.querySelector('#toggle-2');
+  // toggle1.addEventListener('change', handleUIEvent);
+  // toggle2.addEventListener('change', handleUIEvent);
 
-  const knob1 = document.querySelector('#knob-1');
-  const knob2 = document.querySelector('#knob-2');
-  const knob3 = document.querySelector('#knob-3');
-  knob1.addEventListener('input', handleUIEvent);
-  knob1.addEventListener('change', handleUIEvent);
-  knob2.addEventListener('input', handleUIEvent);
-  knob2.addEventListener('change', handleUIEvent);
-  knob3.addEventListener('input', handleUIEvent);
-  knob3.addEventListener('change', handleUIEvent);
+  // const knob1 = document.querySelector('#knob-1');
+  // const knob2 = document.querySelector('#knob-2');
+  // const knob3 = document.querySelector('#knob-3');
+  // knob1.addEventListener('input', handleUIEvent);
+  // knob1.addEventListener('change', handleUIEvent);
+  // knob2.addEventListener('input', handleUIEvent);
+  // knob2.addEventListener('change', handleUIEvent);
+  // knob3.addEventListener('input', handleUIEvent);
+  // knob3.addEventListener('change', handleUIEvent);
 
-  const dropdown1 = document.querySelector('#dropdown-1');
-  dropdown1.options = {
-    'Option A': 'value_a',
-    'Option B': 'value_b',
-    'Option C': 'value_c',
-    'Another Option': 'value_d'
-  };
+  // const dropdown1 = document.querySelector('#dropdown-1');
+  // dropdown1.options = {
+  //   'Option A': 'value_a',
+  //   'Option B': 'value_b',
+  //   'Option C': 'value_c',
+  //   'Another Option': 'value_d'
+  // };
 
-  dropdown1.addEventListener('select', (event) => {
-    console.log('Select event received:', event.detail);
-  });
+  // dropdown1.addEventListener('select', (event) => {
+  //   console.log('Select event received:', event.detail);
+  // });
 
-  const matrix2D = document.querySelector('#matrix-2d-1');
+  const matrix2D = document.querySelector('#sequencer');
   matrix2D.addEventListener('change', (event) => {
     console.log('matrix changed:', event.detail.values);
   });
 
-  const wavetableView1 = document.querySelector('#wavetable-view-1');
-  const sawtooth = WavetableDataSet[35];
-  wavetableView1.setComplexData(sawtooth.real, sawtooth.imag);
+  const wavetableView1 = document.querySelector('#wave-view-1');
+  const wavetableView2 = document.querySelector('#wave-view-2');
+  wavetableView1.setComplexData(WavetableDataSet[35].real, WavetableDataSet[35].imag);
+  wavetableView2.setComplexData(WavetableDataSet[20].real, WavetableDataSet[20].imag);
 };
 
 window.addEventListener('load', initialize);
