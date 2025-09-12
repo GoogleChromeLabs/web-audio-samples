a70d81e3fda6de83eb801dcbb7f6f7bbeaa0bc78
Hongchan Choi <hongchan.choi@gmail.com>
2025-09-12 14:33:06 -0700

    Upgrade site building components (and clean up) (#425)

--- .eleventy.js
+++ .eleventy.js
@@ -2,33 +2,31 @@ const yaml = require('js-yaml');
 const navigationPlugin = require('@11ty/eleventy-navigation');
 const path = require('path');
 
-// Generate the shortened commit hash and create (overwrite) `build_info.json`
-// file. This file will eventually be added to the footer of the page.
-const writeBuildInfoToFile = () => {
-  const version = require('./package.json').version;
-  const commitHash = require('child_process')
-      .execSync('git rev-parse --short HEAD').toString().trim();
-  const commitDate = require('child_process')
-      .execSync('git show -s --format=%cd --date=short').toString().trim();
-  const currentYear = (new Date()).getFullYear();
-  const jsonData = JSON.stringify({
-    version: version,
-    revision: commitHash,
-    lastUpdated: commitDate,
-    copyrightYear: currentYear
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addGlobalData('build_info', () => {
+    const version = require('./package.json').version;
+    const commitHash = require('child_process')
+      .execSync('git rev-parse --short HEAD')
+      .toString()
+      .trim();
+    const commitDate = require('child_process')
+      .execSync('git show -s --format=%cd --date=short')
+      .toString()
+      .trim();
+    const currentYear = new Date().getFullYear();
+    return {
+      version: version,
+      revision: commitHash,
+      lastUpdated: commitDate,
+      copyrightYear: currentYear,
+    };
   });
-  const fs = require('fs');
-  fs.writeFileSync('src/_data/build_info.json', jsonData);
-};
-
-module.exports = function(eleventyConfig) {
-  writeBuildInfoToFile();
 
   // See .eleventyignore for files to ignore.
   eleventyConfig.setUseGitIgnore(false);
 
   // To enable YAML files in `_data`.
-  eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents));
+  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
 
   // To handle relative paths and basic navigation via breadcrumbs.
   eleventyConfig.addPlugin(navigationPlugin);
@@ -82,11 +80,11 @@ module.exports = function(eleventyConfig) {
     'src/sitemap.xml',
     'src/lib/**/*.js',
     'src/lib/**/*.html',
-  ].map(path => eleventyConfig.addPassthroughCopy(path));
+  ].map((path) => eleventyConfig.addPassthroughCopy(path));
 
   // eleventyConfig.addPassthroughCopy('src/favicon');
 
   return {
-    dir: {input: 'src'}
+    dir: { input: 'src' },
   };
 };
--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,5 @@
-const cssNanoProductionOptions = {
-  preset: [
-    'default',
-    {discardComments: {removeAll: true}}
-  ]
-};
-  
 module.exports = ({env}) => ({
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: env === 'production' ? cssNanoProductionOptions : false
+    "@tailwindcss/postcss": {},
   }
 });
